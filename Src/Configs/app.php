4f38b2df1dcf52c9cb757c9301dadd59c5540811
Esteban Flores <ejfr1985@gmail.com>
2019-10-31 18:33:14 -0500

    _Initial config and helper classes

--- Src/Configs/app.php
+++ Src/Configs/app.php
@@ -5,3 +5,10 @@
  * Date: 10/31/2019
  * Time: 5:53 PM
  */
+
+return [
+    'app_name' => 'Bug Report App',
+    'env' => 'local',
+    'debug' => 'true',
+    'log_path' => __DIR__.'/../Logger',
+];
--- Src/Helpers/Config.php
+++ Src/Helpers/Config.php
@@ -6,10 +6,51 @@
  * Time: 5:55 PM
  */
 
+declare(strict_types = 1);
+
 namespace App\Helpers;
 
 
 class Config
 {
+    public static function get(string $filename, string $key = null)
+    {
+        $fileContent = self::getFileContent($filename);
+
+        if ($key == null) {
+            return $fileContent;
+        }
+
+        return isset($fileContent[$key]) ? $fileContent[$key] : [];
+
+    }
+
+    public static function getFileContent(string $filename): array
+    {
+
+        $fileContent = [];
+
+        try {
+
+            $path = realpath(sprintf(__DIR__ . '/../Configs/%s.php', $filename));
+
+            if (file_exists($path)) {
+
+                $fileContent = require $path;
+
+            }
+
+
+        } catch (\Throwable $exception) {
+
+            throw new \RuntimeException(
+                sprintf('The file: %s is not found', $filename)
+            );
+
+        }
+
+        return $fileContent;
+
+    }
 
 }
